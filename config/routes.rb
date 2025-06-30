13157f20a7d26d06556722ecda2b8aa852205136
Demetrious Wilson <demetriouswilson@gmail.com>
2025-06-29 20:03:16 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,12 @@
 PinFlags::Engine.routes.draw do
+  namespace :feature_tags do
+    resources :imports, only: %i[new create]
+    resources :exports, only: %i[index]
+  end
+
+  resources :feature_tags, except: %i[edit] do
+    resources :feature_subscriptions, only: %i[new create destroy], module: :feature_tags
+  end
+
+  root "feature_tags#index"
 end
