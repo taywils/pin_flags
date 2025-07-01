3846eb3723a2ae94b2845705995777c0e807e2c2
Demetrious Wilson <demetriouswilson@gmail.com>
2025-06-30 22:25:12 -0700

    Add feature_tags_controller#create action

--- app/controllers/pin_flags/feature_tags_controller.rb
+++ app/controllers/pin_flags/feature_tags_controller.rb
@@ -14,6 +14,14 @@ module PinFlags
     end
 
     def create
+      @feature_tag = PinFlags::FeatureTag.new(feature_tag_params)
+
+      if @feature_tag.save
+        redirect_to pin_flags.feature_tags_path, notice: "Feature tag created successfully."
+      else
+        flash.now[:alert] = @feature_tag.errors.full_messages.to_sentence
+        render :new
+      end
     end
 
     def update
@@ -23,6 +31,10 @@ module PinFlags
     end
 
     private
+
+    def feature_tag_params
+      params.expect(feature_tag: [ :name, :enabled ])
+    end
 
     def set_search_param
       @search_param = params.fetch(:filter, nil)
