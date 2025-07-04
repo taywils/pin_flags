2ebcb49f257d997c2bbe2721a260e3c416039253
Demetrious Wilson <demetriouswilson@gmail.com>
2025-07-03 22:01:31 -0700

    Add feature_tags_controller actions for new and destroy Add shared error messages partial Add feature_tags view helper methods

--- app/controllers/pin_flags/feature_tags_controller.rb
+++ app/controllers/pin_flags/feature_tags_controller.rb
@@ -1,6 +1,12 @@
 module PinFlags
   class FeatureTagsController < ApplicationController
-    before_action :set_search_param, only: %i[index show]
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+    before_action :set_filter_param, only: %i[index show]
+    before_action :set_feature_taggable_type, only: %i[show]
+    before_action :set_feature_tag, only: %i[show update destroy]
+
+    PER_PAGE ||= 10
 
     def index
       @feature_tags = PinFlags::FeatureTag.all
@@ -17,27 +23,56 @@ module PinFlags
       @feature_tag = PinFlags::FeatureTag.new(feature_tag_params)
 
       if @feature_tag.save
-        redirect_to pin_flags.feature_tags_path, notice: "Feature tag created successfully."
+        @feature_tags = PinFlags::FeatureTag.all
+        handle_success(:create)
       else
-        flash.now[:alert] = @feature_tag.errors.full_messages.to_sentence
-        render :new
+        render :new, status: :unprocessable_content
       end
     end
 
     def update
+      if @feature_tag.update(feature_tag_params)
+        handle_success(:update)
+      else
+        render :index, status: :unprocessable_content
+      end
     end
 
     def destroy
+      @feature_tag.destroy
+      handle_success(:delete)
     end
 
     private
+
+    def set_feature_tag
+      @feature_tag = PinFlags::FeatureTag.find(params[:id])
+    end
 
     def feature_tag_params
       params.expect(feature_tag: [ :name, :enabled ])
     end
 
-    def set_search_param
-      @search_param = params.fetch(:filter, nil)
+    def set_filter_param
+      @set_filter_param = params.fetch(:filter, nil)
+    end
+
+    def set_feature_taggable_type
+      @feature_taggable_type = params.fetch(:feature_taggable_type, nil)
+    end
+
+    def handle_success(action)
+      flash_notice = "Feature tag was successfully #{action}d."
+
+      respond_to do |format|
+        format.html { redirect_to feature_tags_path, notice: flash_notice }
+        format.turbo_stream { flash.now[:notice] = flash_notice }
+      end
+    end
+
+    def handle_record_not_found
+      flash[:error] = "Feature tag not found."
+      redirect_to pin_flags.feature_tags_path, status: :see_other
     end
   end
 end
