55f55f4bdcc60aaec5aa3eababfea1920e50e711
Demetrious Wilson <demetriouswilson@gmail.com>
2025-07-13 10:45:10 -0700

    Fix formatting

--- app/helpers/pin_flags/feature_tags_helper.rb
+++ app/helpers/pin_flags/feature_tags_helper.rb
@@ -1,11 +1,11 @@
 module PinFlags
   module FeatureTagsHelper
-  def display_feature_tag_table_turbo_frame_row_id(feature_tag)
-    "feature_tags_table_row_#{dom_id(feature_tag)}"
-  end
+    def display_feature_tag_table_turbo_frame_row_id(feature_tag)
+      "feature_tags_table_row_#{dom_id(feature_tag)}"
+    end
 
-  def display_feature_tag_row_delete_confirmation(feature_tag)
-    "Are you sure you want to delete the feature tag '#{feature_tag.name}'?"
-  end
+    def display_feature_tag_row_delete_confirmation(feature_tag)
+      "Are you sure you want to delete the feature tag '#{feature_tag.name}'?"
+    end
   end
 end
