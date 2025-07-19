8e34392bf7a12ac9ff2e7cdc956bd15fd263999b
Demetrious Wilson <demetriouswilson@gmail.com>
2025-07-19 11:51:26 -0700

    Bump version to v0.1.1 Make feature_tag migration use 'up' and 'down' methods Make feature_subscriptions migration use 'up' and 'down' methods

--- lib/generators/pin_flags/install/templates/create_feature_tag.rb
+++ lib/generators/pin_flags/install/templates/create_feature_tag.rb
@@ -1,5 +1,5 @@
 class CreateFeatureTag < ActiveRecord::Migration[8.0]
-  def change
+  def up
     create_table :pin_flags_feature_tags do |t|
       t.string :name, null: false
       t.boolean :enabled, default: true, null: false
@@ -8,5 +8,12 @@ class CreateFeatureTag < ActiveRecord::Migration[8.0]
     end
 
     add_index :pin_flags_feature_tags, :name, unique: true
+  end
+
+  def down
+    if table_exists?(:pin_flags_feature_tags)
+      remove_index :pin_flags_feature_tags, :name if index_exists?(:pin_flags_feature_tags, :name)
+      drop_table :pin_flags_feature_tags
+    end
   end
 end
